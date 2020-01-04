3863dfd04aeb7fc0bf399f738a1cc97a963c4787
akram <akram2410@gmail.com>
2020-01-05 00:17:51 +0530

    added collection preview component and styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homePage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shops/shop.component'
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
       </BrowserRouter>
     </div>

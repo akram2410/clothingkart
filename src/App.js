7459ef40526980c36053624191f8916afbe6c0eb
akram <akram2410@gmail.com>
2020-01-04 10:37:16 +0530

    created using CRA and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './App.css';
+import HomePage from './homepage.component'
+
+export class App extends React.Component {
+render(){
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )};
+}
+
+export default App;

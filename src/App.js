e0daa3bec5a9969f628e8ff4dbaccef6fb9b754f
Akram Mohammad <akram_mohammad1@persistent.co.in>
2020-03-18 11:36:53 +0530

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter,Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homePage/homepage.component';
-import ShopPage from './pages/shops/shop.component'
+import HomePage from "./pages/homePage/homepage.component";
+import ShopPage from "./pages/shops/shop.component";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </BrowserRouter>
     </div>
   );

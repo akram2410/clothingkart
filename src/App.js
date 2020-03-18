5b6ce5f3bc67f7453950bb885d19e1a34fce451e
Akram Mohammad <akram_mohammad1@persistent.co.in>
2020-03-18 14:20:27 +0530

    added signIn page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homePage/homepage.component";
-import ShopPage from "./pages/shops/shop.component";
+import ShopPage from "./pages/shops/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
+import SignInAndSignUp from "./pages/signInAndSignUp/signInAndSignUp.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
         </Switch>
       </BrowserRouter>
     </div>

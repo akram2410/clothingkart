b5a50f9c1a155afd074274848be5172af5004765
Akram Mohammad <akram_mohammad1@persistent.co.in>
2020-03-23 10:08:22 +0530

    Updates

---
+++ precache-manifest.67452b8ca8197b1406d401ea7ebf2c0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ec318da22796369e419ae434803c92",
+    "url": "/clothingkart/index.html"
+  },
+  {
+    "revision": "ea33428faef99ebb8dec",
+    "url": "/clothingkart/static/css/main.58f7db0f.chunk.css"
+  },
+  {
+    "revision": "04406affae762eb7af6f",
+    "url": "/clothingkart/static/js/2.a151c3cf.chunk.js"
+  },
+  {
+    "revision": "11899f1c0c2a0a75a4adba8c4d8b243a",
+    "url": "/clothingkart/static/js/2.a151c3cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea33428faef99ebb8dec",
+    "url": "/clothingkart/static/js/main.1896d2ec.chunk.js"
+  },
+  {
+    "revision": "c4cda84e7c218966cafd",
+    "url": "/clothingkart/static/js/runtime-main.d456022d.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/clothingkart/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothingkart/static/media/shopping-bag.30f07071.svg"
+  }
+]);
